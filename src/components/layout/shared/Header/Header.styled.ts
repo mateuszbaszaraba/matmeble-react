82a37f957f9e80ee5018444fdd9f741f23a83117
Mateusz Baszaraba <mateuszbaszaraba@outlook.com>
2022-03-19 13:31:36 +0100

    Updated header for tablet and desktop

--- src/components/layout/shared/Header/Header.styled.ts
+++ src/components/layout/shared/Header/Header.styled.ts
@@ -6,9 +6,12 @@ export const HeaderStyled = styled.header`
   height: 15vh;
   z-index: 1;
   margin-top: var(--minusGlobalMargin);
+  @media only screen and (min-width: 680px) {
+    height: 10vh;
+  }
 `;
 
-export const HeaderBack = styled.header<{ bckg?: string }>`
+export const HeaderBack = styled.header`
   height: 17vh;
   width: 100%;
   background-color: #292929;
@@ -16,4 +19,9 @@ export const HeaderBack = styled.header<{ bckg?: string }>`
   z-index: -3;
   margin-left: var(--minusGlobalMargin);
   margin-top: var(--minusGlobalMargin);
+  @media only screen and (min-width: 680px) {
+    margin: -1.5em -5em;
+    height: 10vh;
+    padding: 1.5em 0;
+  }
 `;
