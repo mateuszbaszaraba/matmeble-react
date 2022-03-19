32afd7985a53c5a48f9117bd980ad688b6b782a8
Mateusz Baszaraba <mateuszbaszaraba@outlook.com>
2022-03-19 13:18:45 +0100

    Created mobile version of products page

--- src/components/layout/ProductsSection/ProductsSection.styled.ts
+++ src/components/layout/ProductsSection/ProductsSection.styled.ts
@@ -2,14 +2,12 @@ import styled from 'styled-components';
 
 export const ProductsSectionStyled = styled.section`
   background-color: var(--lightGreyColor);
-  margin-left: var(--minusGlobalMargin);
-  margin-right: var(--minusGlobalMargin);
+  margin: 2em var(--minusGlobalMargin);
   display: flex;
   gap: 3em;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin-top: 2em;
   padding-top: 5em;
   padding-bottom: 5em;
   clip-path: polygon(0 0, 100% 5%, 100% 100%, 0 95%);
--- src/components/pages/Products.tsx
+++ src/components/pages/Products.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Wrapper from '../layout/shared/Wrapper/Wrapper';
 import ProdInfoSection from '../layout/ProdInfoSection/ProdInfoSection';
 import ProductsSection from '../layout/ProductsSection/ProductsSection';
+import Footer from '../layout/shared/Footer/Footer';
 
 const Products = () => {
   return (
     <Wrapper>
       <ProdInfoSection />
       <ProductsSection />
+      <Footer />
     </Wrapper>
   );
 };
